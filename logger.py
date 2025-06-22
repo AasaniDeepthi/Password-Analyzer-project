467d311cb0ed43959f0bd0e8829f0e770932fbab
AasaniDeepthi <139120953+AasaniDeepthi@users.noreply.github.com>
2025-06-22 21:32:19 +0530

    Delete logger.py

--- logger.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-def log_packet(packet_summary: str, reason: str = ""):
-    """
-    Append a timestamped log entry with packet summary and reason to firewall.og_txt.
-    """
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    with open("firewall.log_txt", "a") as f:
-        f.write(f"{timestamp} - {reason} - {packet_summary}\n")
