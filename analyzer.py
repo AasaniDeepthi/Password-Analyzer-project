f41c716a8995a291796780b29486a151cc1e1ee4
AasaniDeepthi <139120953+AasaniDeepthi@users.noreply.github.com>
2025-06-22 21:43:24 +0530

    Delete analyzer.py

--- analyzer.py
+++
@@ -1,8 +0,0 @@
-
-from zxcvbn import zxcvbn
-
-def analyze_password(password):
-    result = zxcvbn(password)
-    score = result['score']
-    feedback = result['feedback']
-    return score, feedback['warning'], feedback['suggestions']
